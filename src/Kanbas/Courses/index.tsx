dd447729b08b0f159ff5f19e4c8cde174ba51d75
Nanofaux <ilmc57@hotmail.com>
2024-09-24 17:40:11 -0400

    Updates

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,11 +1,12 @@
 import CoursesNavigation from "./Navigation";
 import {Navigate, Route, Routes} from "react-router-dom";
+import Modules from "./Modules";
 
 export default function Courses() {
     return (
         <div id="wd-courses">
             <h2>Course 1234</h2>
-            <hr />
+            <hr/>
             <table>
                 <tr>
                     <td valign={"top"}>
@@ -13,12 +14,12 @@ export default function Courses() {
                     </td>
                     <td valign={"top"}>
                         <Routes>
-                            <Route path="/" element={<Navigate to="Home" />} />
-                            <Route path="Home" element={<h2>Home</h2>} />
-                            <Route path="Modules" element={<h2>Modules</h2>} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
-                            <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
-                            <Route path="People" element={<h2>People</h2>} />
+                            <Route path="/" element={<Navigate to="Home"/>}/>
+                            <Route path="Home" element={<h3>Home</h3>}/>
+                            <Route path="Modules" element={<Modules/>}/>
+                            <Route path="Assignments" element={<h2>Assignments</h2>}/>
+                            <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>}/>
+                            <Route path="People" element={<h2>People</h2>}/>
                         </Routes>
                     </td>
                 </tr>
