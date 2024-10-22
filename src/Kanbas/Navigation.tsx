110f913e8ab333bd137bb2fa7451f0eccb7ded0e
Nanofaux <ilmc57@hotmail.com>
2024-10-22 16:25:27 -0400

    Updates

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,6 +1,20 @@
 import {Link} from "react-router-dom";
+import {useLocation} from "react-router";
+import {AiOutlineDashboard} from "react-icons/ai";
+import {LiaBookSolid, LiaCogSolid} from "react-icons/lia";
+import {FaInbox} from "react-icons/fa6";
+import {IoCalendarOutline} from "react-icons/io5";
 
 export default function KanbasNavigation() {
+    const { pathname } = useLocation();
+    const links = [
+        { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+        { label: "Courses",   path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+        { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
+        { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
+        { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+    ];
+
     return (
         <div id="wd-kanbas-navigation">
             <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,7 @@
-import { useLocation } from "react-router";
+import {useLocation} from "react-router";
+
 export default function TOC() {
-    const { pathname } = useLocation();
+    const {pathname} = useLocation();
     return (
         <ul className="nav nav-pills">
             <li className="nav-item">
