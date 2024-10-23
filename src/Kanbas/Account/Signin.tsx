c3cf1f00c2e7a60aab8b35a3ab6df25d65eba066
Nanofaux <ilmc57@hotmail.com>
2024-10-22 22:47:43 -0400

    Fix signin Button

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,15 +1,18 @@
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 export default function Signin() {
     return (
-        <div id={"wd-signin-screen"}>
-            <h3>Sign in</h3>
-            <input id={"wd-username"} className="form-control mb-2" placeholder={"username"}/>
-            <input id={"wd-password"} className="form-control mb-2" placeholder={"password"} type={"password"}/>
-            <Link to="/Kanbas/Dashboard" className="btn btn-primary w-100 mb-2"
-                  id={"wd-signin-link"}>Sign in</Link><br/>
-            <Link to={"/Kanbas/Account/Signup"} className="btn btn-primary w-100 mb-2"
-                  id={"wd-signup-link"}>Sign up</Link>
+        <div id="wd-signin-screen">
+            <h1>Sign in</h1>
+            <input id="wd-username"
+                   placeholder="username"
+                   className="form-control mb-2"/><br />
+            <input id="wd-password"
+                   placeholder="password" type="password"
+                   className="form-control mb-2"/><br />
+            <Link id="wd-signin-btn"
+                  to="/Kanbas/Account/Profile"
+                  className="btn btn-primary w-100">
+                Sign in </Link><br />
+            <Link id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
         </div>
-    )
-}
+    );}
